c9c010f21d631147f1ab81a2c3203b2bc8a0ed23
Furkan Melih Ercan <99879453+fmelihh@users.noreply.github.com>
2024-02-03 19:04:17 +0300

    feat: hello world example

---
+++ hello_world/target/debug/deps/hello_world-76729573f0258003.d
@@ -0,0 +1,5 @@
+/home/b2metric-engineer/BoostUpProjects/rust-documentation-examples/hello_world/target/debug/deps/hello_world-76729573f0258003: src/main.rs
+
+/home/b2metric-engineer/BoostUpProjects/rust-documentation-examples/hello_world/target/debug/deps/hello_world-76729573f0258003.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/b2metric-engineer/BoostUpProjects/rust-documentation-examples/hello_world/target/debug/hello_world: /home/b2metric-engineer/BoostUpProjects/rust-documentation-examples/hello_world/src/main.rs
